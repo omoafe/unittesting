5ef21802a0c62d8ceda40ee94d5bfb959ad53e50
omoafe <afedanafe@gmail.com>
2023-05-08 02:32:43 -0600

    decorate the test funcs

--- tests/test_math_func.py
+++ tests/test_math_func.py
@@ -1,16 +1,30 @@
 from pipelines import math_func
+import pytest
 
+@pytest.mark.number
 def test_add():
     assert math_func.add(7, 3) == 10
     assert math_func.add(7) == 9
 
+
+@pytest.mark.number
 def test_product():
     assert math_func.product(5, 5) == 25
     assert math_func.product(5) == 10
 
+
+@pytest.mark.strings
 def test_add_strings():
     result = math_func.add('Hello', ' World')
     assert result == 'Hello World'
     assert type(result) is str 
     assert 'Heldlo' not in result
     assert 'Hello' in result
+
+@pytest.mark.strings
+def test_product_strings():
+    assert math_func.product('Hello ', 3) == 'Hello Hello Hello '
+    result = math_func.product('Hello ')
+    assert result == 'Hello Hello '
+    assert type(result) is str
+    
